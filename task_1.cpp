74fb63f08c7ecde2d4e0da630fa276a0eeb1c626
Ivan Ushachov <ushachovg324@gmail.com>
2024-02-28 18:02:18 +0200

    fixed

--- task_1.cpp
+++ task_1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 bool isLeapYear(int year)
 {
-    return (year % 4 == 0);
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
 int daysInMonth(int month, int year)
